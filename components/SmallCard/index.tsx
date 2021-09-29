fc3ed01c1e860b63aaecf0b9001f2fdd2b8f26ea
Heron Rodrigues <heronrs1@gmail.com>
2021-09-28 23:00:44 -0300

    feat: :sparkles: Create SmallCard component

---
+++ components/SmallCard/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+import { styles } from './styles';
+
+export default function SmallCard() {
+  return (
+    <View style={styles.card}>
+        <View style={styles.images}>
+            <Image source={require("../../assets/planet.png")} style={styles.astro} />
+            <Image source={require("../../assets/aim.png")} style={styles.aim} />
+        </View>
+        <View>
+            <Text style={styles.astroTitle}>Mercúrio</Text>
+            <Text style={styles.astroVisibility}>Visível da sua localização</Text>
+        </View>
+    </View>
+  );
+}
---
+++ components/SmallCard/styles.ts
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  card: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    height: 176,
+    width: 176,
+    backgroundColor: '#141A26',
+    borderRadius: 8,
+    overflow: 'hidden',
+    padding: 8
+  },
+
+  images: {
+    alignItems: 'flex-end'
+  },
+
+  astro: {
+    position: "absolute",
+    top: '-30%',
+    left: '-20%',
+    transform: [{ translateY: -30 }],
+    height: 136,
+    width: 136,
+  },
+
+  aim: {
+    height: 24,
+    width: 24,
+  },
+
+  astroTitle: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '500',
+    marginBottom: 8
+  },
+
+  astroVisibility: {
+    fontSize: 13,
+    color: '#fff',
+  }
+});
