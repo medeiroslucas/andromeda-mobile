a5ed10e0745fce4db54631c0fb4b68934a9a74dc
Henrike100 <henrikem100@gmail.com>
2021-10-10 16:51:02 -0300

    Getting location

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { View, ImageBackground, Text, ScrollView, FlatList, ActivityIndicator } from 'react-native';
+import { View, ImageBackground, Text, ScrollView, FlatList, ActivityIndicator, Platform } from 'react-native';
 import AstroCardSmall from '../../components/AstroCardSmall';
-
+import { requestMultiple, PERMISSIONS } from 'react-native-permissions';
+import Geolocation from 'react-native-geolocation-service';
 import { styles } from './styles';
 
 export default function Home() {
-  const [isLoading, setLoading] = useState(true);
+  const emptyList: string[] = [];
+  const emptyCoord = {}
 
-  let planetList: string[] = [];
-  let satelliteList: string[] = [];
+  const [isLoading, setLoading] = useState(true);
+  const [coords, setCoords] = useState(emptyCoord);
+  const [planetList, setPlanetList] = useState(emptyList);
+  const [satelliteList, setSatelliteList] = useState(emptyList);
 
   const getAstros = async () => {
     try {
@@ -27,17 +31,21 @@ export default function Home() {
         "venus": "planet"
       }
 
-      planetList = [];
-      satelliteList = [];
+      var pList: string[] = [];
+      var sList: string[] = [];
 
       for(const astro in response) {
         if(response[astro] === "planet") {
-          planetList.push(astro);
+          pList.push(astro);
         }
         else {
-          satelliteList.push(astro);
+          sList.push(astro);
         }
       }
+
+      setPlanetList(pList);
+      setSatelliteList(sList);
+
     } catch (error) {
       console.error(error);
     } finally {
@@ -45,8 +53,53 @@ export default function Home() {
     }
   }
 
+  const getLocation = async () => {
+    const result = await requestMultiple(
+      [
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION
+      ]).then(
+        (statuses: any) => {
+          const statusFine = statuses[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION];
+          const statusBack = statuses[PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION]; 
+          // Em APIs do Android abaixo da 29 não é necessário permissão para background location,
+          // apenas solicitar acesso ao GPS já oferece tudo que é necessário para utilizar a localização em primeiro e segundo plano.
+          // Nesse caso, apenas verificamos se a autorização do GPS é positiva
+          if (Platform.Version < 29) { 
+            if (statusFine == 'granted') {
+              return true;
+            } else {
+              // 'Usuário não aceitou solicitação de uso do GPS'
+            }
+          }
+          // Caso a API seja > 29, é necessário verificar se ambas as autorizações foram positivas.
+          else {
+            if (statusFine == 'granted' && statusBack == 'granted') {
+              return true;
+            } else {
+              // 'Usuário não aceitou solicitação de uso do GPS'
+            }
+          } 
+        },
+      );
+
+      if (result) {
+        Geolocation.getCurrentPosition(
+          ({ coords }: {coords: {latitude: number, longitude: number}}) => {
+            setCoords({
+              latitude: coords.latitude,
+              longitude: coords.longitude,
+            });
+          }, (error: any) => {
+            // 'Não foi possível obter a localização'
+          }, { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, showLocationDialog: true } 
+        )
+      }
+  }
+
   useEffect(() => {
     getAstros();
+    getLocation();
   }, []);
 
   const renderItem = ({ item } : {item: string}) => {
