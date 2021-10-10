877bebf9af6870ec8f8e1c3ddcdc3907e9e24b8d
Henrike100 <henrikem100@gmail.com>
2021-10-10 15:11:14 -0300

    Communicating with api (astros)

--- components/AstroCardSmall/index.tsx
+++ components/AstroCardSmall/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, Image } from 'react-native';
 
 import { styles } from './styles';
 
-export default function AstroCardSmall() {
+export default function AstroCardSmall(props: {astro: string}) {
   return (
     <View style={styles.card}>
         <View style={styles.images}>
@@ -11,7 +11,7 @@ export default function AstroCardSmall() {
             <Image source={require("../../assets/aim.png")} style={styles.aim} />
         </View>
         <View>
-            <Text style={styles.astroTitle}>Mercúrio</Text>
+            <Text style={styles.astroTitle}>{props.astro.charAt(0).toUpperCase() + props.astro.slice(1)}</Text>
             <Text style={styles.astroVisibility}>Visível da sua localização</Text>
         </View>
     </View>
--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
-import { View, ImageBackground, Text, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ImageBackground, Text, ScrollView, FlatList, ActivityIndicator } from 'react-native';
 import AstroCardSmall from '../../components/AstroCardSmall';
 
 import { styles } from './styles';
 
 export default function Home() {
+  const [isLoading, setLoading] = useState(true);
+
+  let planetList: string[] = [];
+  let satelliteList: string[] = [];
+
+  const getAstros = async () => {
+    try {
+      //const response = await fetch('link');
+      //const json = await response.json();
+      //setData(json);
+
+      const response: Record<string, string> = {
+        "jupiter": "planet",
+        "mars": "planet",
+        "mercury": "planet",
+        "moon": "satellite",
+        "neptune": "planet",
+        "saturn": "planet",
+        "uranus": "planet",
+        "venus": "planet"
+      }
+
+      planetList = [];
+      satelliteList = [];
+
+      for(const astro in response) {
+        if(response[astro] === "planet") {
+          planetList.push(astro);
+        }
+        else {
+          satelliteList.push(astro);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getAstros();
+  }, []);
+
+  const renderItem = ({ item } : {item: string}) => {
+    return (
+      <AstroCardSmall astro={item}/>
+    );
+  }
+
   return (
     <ImageBackground source={{uri: "https://andromeda-pi2.s3.sa-east-1.amazonaws.com/background.png"}} resizeMode="cover" style={styles.image}>
       <ScrollView style={styles.container}>
@@ -12,65 +62,25 @@ export default function Home() {
           <Text style={styles.welcomeCaption}>O que vamos explorar hoje?</Text>
           <Text style={styles.categoriesPresentation}>Navegue por Categorias</Text>
 
-          <View style={styles.categoryContainer}>
-            <Text style={styles.categoryTitle}>Planetas</Text>
-            <ScrollView horizontal>
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-            </ScrollView>
-          </View>
+          {isLoading ? <ActivityIndicator/> : (
+            <View style={styles.categoryContainer}>
+              <Text style={styles.categoryTitle}>Planetas</Text>
+              <FlatList
+                data={planetList}
+                renderItem={renderItem}
+              />
+            </View>
+          )}
 
-          <View style={styles.categoryContainer}>
-            <Text style={styles.categoryTitle}>Satélites</Text>
-            <ScrollView horizontal>
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-            </ScrollView>
-          </View>
-
-          <View style={styles.categoryContainer}>
-            <Text style={styles.categoryTitle}>Cometas</Text>
-            <ScrollView horizontal>
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-            </ScrollView>
-          </View>
-
-          <View style={styles.categoryContainer}>
-            <Text style={styles.categoryTitle}>Planetas</Text>
-            <ScrollView horizontal>
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-            </ScrollView>
-          </View>
-
-          <View style={styles.categoryContainer}>
-            <Text style={styles.categoryTitle}>Planetas</Text>
-            <ScrollView horizontal>
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-              <AstroCardSmall />
-            </ScrollView>
-          </View>
+          {isLoading ? <ActivityIndicator/> : (
+            <View style={styles.categoryContainer}>
+              <Text style={styles.categoryTitle}>Satélites</Text>
+              <FlatList
+                data={satelliteList}
+                renderItem={renderItem}
+              />
+            </View>
+          )}
       </ScrollView>
     </ImageBackground>
   );
