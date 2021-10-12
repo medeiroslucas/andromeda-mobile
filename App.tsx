a8f8457f6f1c05952f5943ed5999abb39ff747d7
Heron Rodrigues <heronrs1@gmail.com>
2021-10-12 13:04:23 -0300

    feat: :sparkles: Create bottom navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Search as SearchIcon, Home as HomeIcon } from 'react-native-feather';
 import Search from './pages/Search';
+import Home from './pages/Home';
+import Details from './pages/Details';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar/>
-      <Search />    
+      <StatusBar />
+      <NavigationContainer>
+        <Tab.Navigator 
+          screenOptions={{
+            headerShown: false, 
+            // tabBarBackground: () => (
+              
+        }}>
+          <Tab.Screen 
+            name="Detalhes" 
+            component={Details}
+            options={{
+              tabBarIcon: () => (
+                <SearchIcon color="black" />
+              ),
+            }} 
+          />
+          <Tab.Screen 
+            name="Início" 
+            component={Home}
+            options={{
+              tabBarIcon: () => (
+                <HomeIcon color="black" />
+              ),
+            }}  
+          />
+          <Tab.Screen 
+            name="Buscar" 
+            component={Search}
+            options={{
+              tabBarIcon: () => (
+                <SearchIcon color="black" />
+              ),
+            }} 
+          />
+        </Tab.Navigator>  
+      </NavigationContainer>
     </>
   );
 }
